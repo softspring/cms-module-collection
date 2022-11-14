2c92e51dbbc8ca20a1c74c52c00dc9bda2a4750e
ebpe <ebelmonte82@gmail.com>
2022-11-14 15:14:06 +0100

    Improvements in wysiwyg editor

--- assets/modules/wysiwyg.js
+++ assets/modules/wysiwyg.js
@@ -12,7 +12,38 @@ function delegate(el, evt, sel, handler) {
     }
   });
 }
+
+const initTinyMCE =  function(wyysiwyg, content) {
+  document.getElementById(wyysiwyg.getAttribute('id')).setAttribute('value',content);
+    tinymce.init({
+      selector: '#'+wyysiwyg.getAttribute('id'),
+      height: 300,
+      menubar: false,
+      plugins:
+        'code ' +
+        'image link media template code ' +
+        'table charmap pagebreak nonbreaking anchor ' +
+        'insertdatetime advlist lists wordcount help ' +
+        'charmap quickbars emoticons',
+      toolbar: 'undo redo | bold italic underline strikethrough |' +
+                ' alignleft aligncenter alignright alignjustify |' +
+                ' outdent indent |  numlist bullist | forecolor removeformat | ' +
+                ' link anchor code | ltr rtl | fontsize blocks | ' +
+                ' charmap emoticons',
+      toolbar_sticky: false,
+      extended_valid_elements:"svg[*],defs[*],pattern[*],desc[*],metadata[*],g[*],mask[*],path[*],line[*],marker[*],rect[*],circle[*],i[*]",
+      setup: function (editor) {
+      editor.on('init', function (e) {
+        editor.setContent(content);
+      });
+    }
+  });
+};
+
 window.addEventListener('load', function () {
+
+  let description = [];
+
   if (typeof tinyMCE !== 'undefined') {
     delegate(document, 'hidden.bs.modal', '[data-wysiwyg-modal]', function(event) {
       var modal = event.target,
@@ -23,6 +54,8 @@ window.addEventListener('load', function () {
         let textCont = moduleCont.querySelector('[data-edit-content-target="' + field + '"]'),
             defaultText = textCont.getAttribute('data-default-text');
         textCont.innerHTML = (tinyMCE.activeEditor.getContent() == '' ? defaultText : tinyMCE.activeEditor.getContent());
+        modal.querySelector('#' + tinyMCE.activeEditor.id).setAttribute('data-initial-value', tinyMCE.activeEditor.getContent());
+        modal.querySelector('#' + tinyMCE.activeEditor.id).setAttribute('value', tinyMCE.activeEditor.getContent());
       }
     });
 
@@ -46,5 +79,82 @@ window.addEventListener('load', function () {
       }
     });
 
+    document.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+      initTinyMCE(element, element.getAttribute('data-initial-value'));
+    });
+
+    document.addEventListener('cms.module.add', function (e) {
+      const module = e.detail.module;
+
+      module.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        initTinyMCE(element, element.getAttribute('data-initial-value'));
+      });
+    });
+
+    document.addEventListener('cms.module.move.down.before', function (e) {
+      const module = e.detail.module,
+          index = parseInt(module.getAttribute('data-index'));
+      description = [];
+
+      description[index] = [];
+      module.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        description[index][element.getAttribute('data-edit-content-input')] = tinymce.get(element.getAttribute('id')).getContent();
+        tinymce.remove('#'+element.getAttribute('id'));
+      });
+      description[index+1] = [];
+      module.closest('.polymorphic-collection-widget').querySelector('[data-index="'+ (index+1) +'"]').querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        description[index+1][element.getAttribute('data-edit-content-input')] = tinymce.get(element.getAttribute('id')).getContent();
+        tinymce.remove('#'+element.getAttribute('id'));
+      });
+      console.log(description);
+    });
+
+    document.addEventListener('cms.module.move.down', function (e) {
+      const module = e.detail.module,
+          index = parseInt(module.getAttribute('data-index'));
+
+      module.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        let content = description[index-1][element.getAttribute('data-edit-content-input')];
+        initTinyMCE(element,content);
+      });
+
+      module.closest('.polymorphic-collection-widget').querySelector('[data-index="'+ (index-1) +'"]').querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        let content = description[index][element.getAttribute('data-edit-content-input')];
+        initTinyMCE(element,content);
+      });
+    });
+
+    document.addEventListener('cms.module.move.up.before', function (e) {
+      const module = e.detail.module,
+          index = parseInt(module.getAttribute('data-index'));
+      description = [];
+
+      description[index] = [];
+      module.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        description[index][element.getAttribute('data-edit-content-input')] = tinymce.get(element.getAttribute('id')).getContent();
+        tinymce.remove('#'+element.getAttribute('id'));
+      });
+
+      description[index-1] = [];
+      module.closest('.polymorphic-collection-widget').querySelector('[data-index="'+ (index-1) +'"]').querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+       description[index-1][element.getAttribute('data-edit-content-input')] = tinymce.get(element.getAttribute('id')).getContent();
+        tinymce.remove('#'+element.getAttribute('id'));
+      });
+    });
+
+    document.addEventListener('cms.module.move.up', function (e) {
+      const module = e.detail.module,
+          index = parseInt(module.getAttribute('data-index'));
+
+      module.querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        let content = description[index+1][element.getAttribute('data-edit-content-input')]
+        initTinyMCE(element,content);
+      });
+
+      module.closest('.polymorphic-collection-widget').querySelector('[data-index="'+ (index+1) +'"]').querySelectorAll('[data-wysiwyg]').forEach(function(element) {
+        let content = description[index][element.getAttribute('data-edit-content-input')]
+        initTinyMCE(element,content);
+      });
+    });
   }
 }, false);
