376b63c0887bd89fab5e29d7b047bec840b05186
ebpe <ebelmonte82@gmail.com>
2023-01-17 13:09:39 +0100

    add options to carousel module

--- assets/modules/carousel.js
+++ assets/modules/carousel.js
@@ -4,7 +4,7 @@
 // import { Navigation, Pagination } from './swiper/swiper-bundle';
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from './swiper';
+import Swiper, { Navigation, Pagination, Autoplay } from './swiper';
 // import Swiper and modules styles
 import './swiper/swiper.scss';
 import './swiper/modules/navigation/navigation.scss';
@@ -13,26 +13,63 @@ import './_carousel.scss';
 
 class Carousel {
     constructor() {
+        this.swippers = [].slice.call(document.querySelectorAll('[data-swipper]'));
+        if (!this.swippers.length) return;
+
         this.init();
     }
 
     async init() {
+        const that = this;
         document.addEventListener("DOMContentLoaded", function() {
-            // init Swiper:
-            const swiper = new Swiper('.swiper', {
-                // configure Swiper to use modules
-                modules: [Navigation, Pagination],
-                loop: true,
-                slidesPerView: window.slidesPerViewCaousel,
-                // If we need pagination
-                pagination: {
-                    el: '.swiper-pagination',
-                },
-                // Navigation arrows
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
+            that.swippers.forEach(function (swipper,i) {
+                const slidesPerViewCarousel = (swipper.getAttribute('data-slides-per-view') == 'auto') ? 'auto' : parseInt(swipper.getAttribute('data-slides-per-view'));
+                const autoplayCarousel = parseInt(swipper.getAttribute('data-autoplay'));
+                const infiniteLoopCarousel = parseInt(swipper.getAttribute('data-infinite-loop'));
+                const classSwiper = 'swiper-' + i;
+                swipper.classList.add(classSwiper);
+
+                // init Swiper:
+                if(autoplayCarousel) {
+                    const swiper = new Swiper('.'+ classSwiper, {
+                        // configure Swiper to use modules
+                        modules: [Navigation, Pagination],
+                        loop: infiniteLoopCarousel,
+                        slidesPerView: slidesPerViewCarousel,
+                        autoplay: {
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        },
+                        // If we need pagination
+                        pagination: {
+                            el: '.swiper-pagination',
+                            clickable: true,
+                        },
+                        // Navigation arrows
+                        navigation: {
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev',
+                        },
+                    });
+                } else {
+                    const swiper = new Swiper('.'+ classSwiper, {
+                        // configure Swiper to use modules
+                        modules: [Navigation, Pagination],
+                        loop: infiniteLoopCarousel,
+                        slidesPerView: slidesPerViewCarousel,
+                        // If we need pagination
+                        pagination: {
+                            el: '.swiper-pagination',
+                            clickable: true,
+                        },
+                        // Navigation arrows
+                        navigation: {
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev',
+                        },
+                    });
+                }
+
             });
         });
     }
