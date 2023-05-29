29e62060e0025983e9036911fd5e510265b5d4e6
Javi H. Gil <javihgil@softspring.es>
2023-05-29 23:50:34 +0200

    Add modules.js and modules.scss to simplify loading

---
+++ assets/modules.js
@@ -0,0 +1,4 @@
+import './../vendor/softspring/cms-module-collection/assets/modules/carousel';
+import './../vendor/softspring/cms-module-collection/assets/modules/hero';
+import './../vendor/softspring/cms-module-collection/assets/modules/title';
+import './../vendor/softspring/cms-module-collection/assets/modules/wysiwyg';
